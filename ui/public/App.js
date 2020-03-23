155bc751aee1f6cfccabea929baf4d9737ff34dd
vandanasdev <60491551+vandanasdev@users.noreply.github.com>
2020-03-22 17:17:42 -0700

    Added uiserver file,.babelrc, ui package.json

--- ui/public/App.js
+++ ui/public/App.js
@@ -1,157 +1,289 @@
-class ProductRow extends React.Component {
-  render() {
-    const product = this.props.product;
-    return React.createElement("tr", null, React.createElement("td", null, product.pname), React.createElement("td", null, "$" + product.price), React.createElement("td", null, product.category), React.createElement("td", null, React.createElement("a", {
-      target: "_blank",
-      href: product.imageUrl
-    }, "View")));
+"use strict";
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _createSuper(Derived) { return function () { var Super = _getPrototypeOf(Derived), result; if (_isNativeReflectConstruct()) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var ProductRow = /*#__PURE__*/function (_React$Component) {
+  _inherits(ProductRow, _React$Component);
+
+  var _super = _createSuper(ProductRow);
+
+  function ProductRow() {
+    _classCallCheck(this, ProductRow);
+
+    return _super.apply(this, arguments);
   }
 
-}
+  _createClass(ProductRow, [{
+    key: "render",
+    value: function render() {
+      var product = this.props.product;
+      return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, product.pname), /*#__PURE__*/React.createElement("td", null, "$" + product.price), /*#__PURE__*/React.createElement("td", null, product.category), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("a", {
+        target: "_blank",
+        href: product.imageUrl
+      }, "View")));
+    }
+  }]);
 
-class ProductTable extends React.Component {
-  render() {
-    const productRows = this.props.products.map(product => React.createElement(ProductRow, {
-      key: product.id,
-      product: product
-    }));
-    return React.createElement("table", {
-      className: "bordered-table"
-    }, React.createElement("thead", null, React.createElement("tr", null, React.createElement("th", null, "Product Name"), React.createElement("th", null, "Price"), React.createElement("th", null, "Category"), React.createElement("th", null, "Image"))), React.createElement("tbody", null, productRows));
+  return ProductRow;
+}(React.Component);
+
+var ProductTable = /*#__PURE__*/function (_React$Component2) {
+  _inherits(ProductTable, _React$Component2);
+
+  var _super2 = _createSuper(ProductTable);
+
+  function ProductTable() {
+    _classCallCheck(this, ProductTable);
+
+    return _super2.apply(this, arguments);
   }
 
-}
+  _createClass(ProductTable, [{
+    key: "render",
+    value: function render() {
+      var productRows = this.props.products.map(function (product) {
+        return /*#__PURE__*/React.createElement(ProductRow, {
+          key: product.id,
+          product: product
+        });
+      });
+      return /*#__PURE__*/React.createElement("table", {
+        className: "bordered-table"
+      }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "Product Name"), /*#__PURE__*/React.createElement("th", null, "Price"), /*#__PURE__*/React.createElement("th", null, "Category"), /*#__PURE__*/React.createElement("th", null, "Image"))), /*#__PURE__*/React.createElement("tbody", null, productRows));
+    }
+  }]);
 
-class ProductAdd extends React.Component {
-  constructor() {
-    super();
-    this.handleSubmit = this.handleSubmit.bind(this);
+  return ProductTable;
+}(React.Component);
+
+var ProductAdd = /*#__PURE__*/function (_React$Component3) {
+  _inherits(ProductAdd, _React$Component3);
+
+  var _super3 = _createSuper(ProductAdd);
+
+  function ProductAdd() {
+    var _this;
+
+    _classCallCheck(this, ProductAdd);
+
+    _this = _super3.call(this);
+    _this.handleSubmit = _this.handleSubmit.bind(_assertThisInitialized(_this));
+    return _this;
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    const form = document.forms.ProductAdd;
-    const priceValue = form.price.value.slice(1);
-    const product = {
-      category: form.category.value,
-      price: priceValue,
-      pname: form.pname.value,
-      imageUrl: form.imageUrl.value
-    };
-    this.props.createProduct(product);
-    form.price.value = "$";
-    form.pname.value = "";
-    form.imageUrl.value = "";
-    form.category.value = "";
-  }
+  _createClass(ProductAdd, [{
+    key: "handleSubmit",
+    value: function handleSubmit(e) {
+      e.preventDefault();
+      var form = document.forms.ProductAdd;
+      var priceValue = form.price.value.slice(1);
+      var product = {
+        category: form.category.value,
+        price: priceValue,
+        pname: form.pname.value,
+        imageUrl: form.imageUrl.value
+      };
+      this.props.createProduct(product);
+      form.price.value = "$";
+      form.pname.value = "";
+      form.imageUrl.value = "";
+      form.category.value = "";
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return /*#__PURE__*/React.createElement("form", {
+        name: "ProductAdd",
+        onSubmit: this.handleSubmit
+      }, /*#__PURE__*/React.createElement("section", {
+        className: "grid-1"
+      }, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Category "), /*#__PURE__*/React.createElement("select", {
+        id: "category"
+      }, /*#__PURE__*/React.createElement("option", {
+        disabled: true,
+        selected: true,
+        value: ""
+      }, " -- Select a product -- "), /*#__PURE__*/React.createElement("option", {
+        value: "Shirts"
+      }, "Shirts"), /*#__PURE__*/React.createElement("option", {
+        value: "Jeans"
+      }, "Jeans"), /*#__PURE__*/React.createElement("option", {
+        value: "Jackets"
+      }, "Jackets"), /*#__PURE__*/React.createElement("option", {
+        value: "Sweaters"
+      }, "Sweaters"), /*#__PURE__*/React.createElement("option", {
+        value: "Accessories"
+      }, "Accesories"))), /*#__PURE__*/React.createElement("div", {
+        id: "price"
+      }, /*#__PURE__*/React.createElement("label", null, "Price Per Unit"), /*#__PURE__*/React.createElement("input", {
+        type: "text",
+        name: "price",
+        defaultValue: "$"
+      })), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Product Name "), /*#__PURE__*/React.createElement("input", {
+        type: "text",
+        name: "pname"
+      })), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Image URL "), /*#__PURE__*/React.createElement("input", {
+        type: "text",
+        name: "imageUrl"
+      })), /*#__PURE__*/React.createElement("button", {
+        type: "submit",
+        className: "butn"
+      }, " Add Product")));
+    }
+  }]);
 
-  render() {
-    return React.createElement("form", {
-      name: "ProductAdd",
-      onSubmit: this.handleSubmit
-    }, React.createElement("section", {
-      className: "grid-1"
-    }, React.createElement("div", null, React.createElement("label", null, "Category "), React.createElement("select", {
-      id: "category"
-    }, React.createElement("option", {
-      disabled: true,
-      selected: true,
-      value: ""
-    }, " -- Select a product -- "), React.createElement("option", {
-      value: "Shirts"
-    }, "Shirts"), React.createElement("option", {
-      value: "Jeans"
-    }, "Jeans"), React.createElement("option", {
-      value: "Jackets"
-    }, "Jackets"), React.createElement("option", {
-      value: "Sweaters"
-    }, "Sweaters"), React.createElement("option", {
-      value: "Accessories"
-    }, "Accesories"))), React.createElement("div", {
-      id: "price"
-    }, React.createElement("label", null, "Price Per Unit"), React.createElement("input", {
-      type: "text",
-      name: "price",
-      defaultValue: "$"
-    })), React.createElement("div", null, React.createElement("label", null, "Product Name "), React.createElement("input", {
-      type: "text",
-      name: "pname"
-    })), React.createElement("div", null, React.createElement("label", null, "Image URL "), React.createElement("input", {
-      type: "text",
-      name: "imageUrl"
-    })), React.createElement("button", {
-      type: "submit",
-      className: "butn"
-    }, " Add Product")));
-  }
+  return ProductAdd;
+}(React.Component);
 
-}
+var ProductList = /*#__PURE__*/function (_React$Component4) {
+  _inherits(ProductList, _React$Component4);
 
-class ProductList extends React.Component {
-  constructor() {
-    super();
-    this.state = {
+  var _super4 = _createSuper(ProductList);
+
+  function ProductList() {
+    var _this2;
+
+    _classCallCheck(this, ProductList);
+
+    _this2 = _super4.call(this);
+    _this2.state = {
       products: []
     };
-    this.createProduct = this.createProduct.bind(this);
+    _this2.createProduct = _this2.createProduct.bind(_assertThisInitialized(_this2));
+    return _this2;
   }
 
-  componentDidMount() {
-    //call to listData so that data is displayed in the list even after the page is refreshed
-    this.listData();
-  }
+  _createClass(ProductList, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      //call to listData so that data is displayed in the list even after the page is refreshed
+      this.listData();
+    }
+  }, {
+    key: "listData",
+    value: function () {
+      var _listData = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+        var query, response, result;
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                query = "query {\n          productList {\n            id category pname price imageUrl\n          }\n        }";
+                _context.next = 3;
+                return fetch('http://localhost:3000/graphql', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                    query: query
+                  })
+                });
 
-  async listData() {
-    const query = `query {
-          productList {
-            id category pname price imageUrl
-          }
-        }`;
-    const response = await fetch('/graphql', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        query
-      })
-    });
-    const result = await response.json();
-    this.setState({
-      products: result.data.productList
-    });
-  }
+              case 3:
+                response = _context.sent;
+                _context.next = 6;
+                return response.json();
 
-  async createProduct(product) {
-    const query = `mutation productAdd($product: ProductInputs!){
-            productAdd(product: $product){
-                id
+              case 6:
+                result = _context.sent;
+                this.setState({
+                  products: result.data.productList
+                });
+
+              case 8:
+              case "end":
+                return _context.stop();
             }
-        }`;
-    const response = await fetch('/graphql', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        query,
-        variables: {
-          product
-        }
-      })
-    });
-    this.listData();
-  }
+          }
+        }, _callee, this);
+      }));
 
-  render() {
-    return React.createElement(React.Fragment, null, React.createElement("h1", null, "My Company Inventory"), React.createElement("div", null, "Showing all available products"), React.createElement("hr", null), React.createElement(ProductTable, {
-      products: this.state.products
-    }), React.createElement("br", null), React.createElement("div", null, " Add a new product to the inventory "), React.createElement("hr", null), React.createElement(ProductAdd, {
-      createProduct: this.createProduct
-    }));
-  }
+      function listData() {
+        return _listData.apply(this, arguments);
+      }
 
-}
+      return listData;
+    }()
+  }, {
+    key: "createProduct",
+    value: function () {
+      var _createProduct = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(product) {
+        var query, response;
+        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                query = "mutation productAdd($product: ProductInputs!){\n            productAdd(product: $product){\n                id\n            }\n        }";
+                _context2.next = 3;
+                return fetch('http://localhost:3000/graphql', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                    query: query,
+                    variables: {
+                      product: product
+                    }
+                  })
+                });
 
-const element = React.createElement(ProductList, null);
+              case 3:
+                response = _context2.sent;
+                this.listData();
+
+              case 5:
+              case "end":
+                return _context2.stop();
+            }
+          }
+        }, _callee2, this);
+      }));
+
+      function createProduct(_x) {
+        return _createProduct.apply(this, arguments);
+      }
+
+      return createProduct;
+    }()
+  }, {
+    key: "render",
+    value: function render() {
+      return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "My Company Inventory"), /*#__PURE__*/React.createElement("div", null, "Showing all available products"), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(ProductTable, {
+        products: this.state.products
+      }), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("div", null, " Add a new product to the inventory "), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(ProductAdd, {
+        createProduct: this.createProduct
+      }));
+    }
+  }]);
+
+  return ProductList;
+}(React.Component);
+
+var element = /*#__PURE__*/React.createElement(ProductList, null);
 ReactDOM.render(element, document.getElementById('myCompInventory'));
