cc2c2172bc1f3337c586d4e863d1aa60ed026003
vandanasdev <60491551+vandanasdev@users.noreply.github.com>
2020-03-22 20:23:26 -0700

    update

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -7,16 +7,16 @@ const app = express();
 app.use(express.static('public'));
 
 
-const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT ||'http://localhost:3000/graphql';
+const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT || 'http://localhost:3000/graphql';
 const env = { UI_API_ENDPOINT };
 
-app.get('/env.js', function(req, res) {
-  res.send(`window.ENV = ${JSON.stringify(env)}`)
-})
+app.get('/env.js', (req, res) => {
+  res.send(`window.ENV = ${JSON.stringify(env)}`);
+});
 
 const port = process.env.UI_SERVER_PORT || 8000;
 
 
-app.listen(8000, function () {
+app.listen(8000, () => {
   console.log(`UI started on port ${port}`);
 });
