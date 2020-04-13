ffda3041e8e1a3d7abd62c22fb938c564873ff1a
vandanasdev <60491551+vandanasdev@users.noreply.github.com>
2020-04-12 20:39:52 -0700

    Separated ProducrAdd, ProductList,ProductTable

---
+++ ui/public/ProductTable.js
@@ -0,0 +1,29 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = ProductTable;
+
+/* globals React */
+function ProductRow(_ref) {
+  var product = _ref.product;
+  return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, product.pname), /*#__PURE__*/React.createElement("td", null, "$".concat(product.price)), /*#__PURE__*/React.createElement("td", null, product.category), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("a", {
+    target: "_blank",
+    href: product.imageUrl
+  }, "View")));
+}
+
+function ProductTable(_ref2) {
+  var products = _ref2.products;
+  // eslint-disable-next-line react/destructuring-assignment
+  var productRows = products.map(function (product) {
+    return /*#__PURE__*/React.createElement(ProductRow, {
+      key: product.id,
+      product: product
+    });
+  });
+  return /*#__PURE__*/React.createElement("table", {
+    className: "bordered-table"
+  }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "Product Name"), /*#__PURE__*/React.createElement("th", null, "Price"), /*#__PURE__*/React.createElement("th", null, "Category"), /*#__PURE__*/React.createElement("th", null, "Image"))), /*#__PURE__*/React.createElement("tbody", null, productRows));
+}
---
+++ ui/src/ProductAdd.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable max-len */
+/* globals React PropTypes */
+
+export default class ProductAdd extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = document.forms.ProductAdd;
+    const priceValue = (form.price.value).slice(1);
+    const product = {
+      category: form.category.value, price: priceValue, pname: form.pname.value, imageUrl: form.imageUrl.value,
+    };
+      // eslint-disable-next-line react/destructuring-assignment
+    this.props.createProduct(product);
+    form.price.value = '$'; form.pname.value = ''; form.imageUrl.value = ''; form.category.value = '';
+  }
+
+  render() {
+    return (
+      <form name="ProductAdd" onSubmit={this.handleSubmit}>
+        <section className="grid-1">
+          <div>
+            <label>Category </label>
+            <select id="category">
+              <option disabled selected value=""> -- Select a product -- </option>
+              <option value="Shirts">Shirts</option>
+              <option value="Jeans">Jeans</option>
+              <option value="Jackets">Jackets</option>
+              <option value="Sweaters">Sweaters</option>
+              <option value="Accessories">Accesories</option>
+            </select>
+          </div>
+          <div id="price">
+            <label>Price Per Unit</label>
+            <input type="text" name="price" defaultValue="$" />
+          </div>
+          <div>
+            <label>Product Name </label>
+            <input type="text" name="pname" />
+          </div>
+          <div>
+            <label>Image URL </label>
+            <input type="text" name="imageUrl" />
+          </div>
+          <button type="submit" className="butn"> Add Product</button>
+        </section>
+
+      </form>
+    );
+  }
+}
