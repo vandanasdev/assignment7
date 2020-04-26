dece679ab00f7aaf9e3a453dcbdc62ccb53dd335
vandanasdev <60491551+vandanasdev@users.noreply.github.com>
2020-04-26 06:07:25 -0700

    Added NavBar and Report page

--- ui/src/Page.jsx
+++ ui/src/Page.jsx
@@ -1,11 +1,72 @@
 import React from 'react';
+import {
+  Navbar, Nav, NavItem, NavDropdown,
+  MenuItem, Glyphicon, Tooltip, OverlayTrigger,
+} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
 import Contents from './Contents.jsx';
+
+function NavBar() {
+  return (
+    <Navbar fluid>
+      <Navbar.Header>
+        <Navbar.Brand>My Company Inventory</Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <LinkContainer exact to="/">
+          <NavItem>Home</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/products">
+          <NavItem>Product List</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/report">
+          <NavItem>Report</NavItem>
+        </LinkContainer>
+      </Nav>
+      <Nav pullRight>
+        <NavItem>
+          <OverlayTrigger
+            placement="left"
+            delayShow={1000}
+            overlay={<Tooltip id="add-product">Add Product</Tooltip>}
+          >
+            <Glyphicon glyph="plus" />
+          </OverlayTrigger>
+        </NavItem>
+        <NavDropdown
+          id="user-dropdown"
+          title={<Glyphicon glyph="option-vertical" />}
+          noCaret
+        >
+          <MenuItem>About</MenuItem>
+        </NavDropdown>
+      </Nav>
+    </Navbar>
+  );
+}
+
+function Footer() {
+  return (
+    <small>
+      <p className="text-center">
+        Full source code available at this
+        {' '}
+        <a href="https://github.com/vandanasdev/assignment6">
+          GitHub repository
+        </a>
+      </p>
+    </small>
+  );
+}
 
 
 export default function Page() {
   return (
     <div>
+      <NavBar />
       <Contents />
+      <Footer />
     </div>
   );
 }
