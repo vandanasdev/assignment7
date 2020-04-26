9534889a8aea89cd08a1ad8ee1b9b9f8e1d70848
vandanasdev <60491551+vandanasdev@users.noreply.github.com>
2020-04-26 14:15:34 -0700

    Added Modal for AddProduct

---
+++ ui/src/ProductAddNavItem.jsx
@@ -0,0 +1,139 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import {
+  NavItem, Glyphicon, Modal, Form, FormGroup, FormControl, ControlLabel,
+  Button, ButtonToolbar, Tooltip, OverlayTrigger,
+} from 'react-bootstrap';
+
+import graphQLFetch from './graphQLFetch.js';
+import Toast from './Toast.jsx';
+
+class ProductAddNavItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showing: false,
+      toastVisible: false,
+      toastMessage: '',
+      toastType: 'success',
+    };
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.showError = this.showError.bind(this);
+    this.dismissToast = this.dismissToast.bind(this);
+  }
+
+  showModal() {
+    this.setState({ showing: true });
+  }
+
+  hideModal() {
+    this.setState({ showing: false });
+  }
+
+  showError(message) {
+    this.setState({
+      toastVisible: true, toastMessage: message, toastType: 'danger',
+    });
+  }
+
+  dismissToast() {
+    this.setState({ toastVisible: false });
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+    this.hideModal();
+    const form = document.forms.ProductAdd;
+    const priceValue = (form.price.value).slice(1);
+    const product = {
+      category: form.category.value,
+      price: priceValue,
+      pname: form.pname.value,
+      imageUrl: form.imageUrl.value,
+    };
+
+    const query = `mutation productAdd($product: ProductInputs!){
+              productAdd(product: $product){
+                  id
+              }
+          }`;
+
+    const data = await graphQLFetch(query, { product }, this.showError);
+    if (data) {
+      const { history } = this.props;
+      history.push(`/edit/${data.productAdd.id}`);
+    }
+  }
+
+  render() {
+    const { showing } = this.state;
+    const { toastVisible, toastMessage, toastType } = this.state;
+    return (
+      <React.Fragment>
+        <NavItem onClick={this.showModal}>
+          <OverlayTrigger
+            placement="left"
+            delayShow={1000}
+            overlay={<Tooltip id="add-product">Add Issue</Tooltip>}
+          >
+            <Glyphicon glyph="plus" />
+          </OverlayTrigger>
+        </NavItem>
+        <Modal keyboard show={showing} onHide={this.hideModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add Product</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form name="productAdd">
+              <FormGroup>
+                <ControlLabel>Category</ControlLabel>
+                <FormControl componentClass="select" name="category" autoFocus>
+                  <option value="Shirts">Shirts</option>
+                  <option value="Jeans">Jeans</option>
+                  <option value="Jackets">Jackets</option>
+                  <option value="Sweaters">Sweaters</option>
+                  <option value="Accessories">Accesories</option>
+                </FormControl>
+              </FormGroup>
+              <FormGroup>
+                <ControlLabel>Product Name</ControlLabel>
+                <FormControl name="pname" />
+              </FormGroup>
+              <FormGroup>
+                <ControlLabel>Price</ControlLabel>
+                <FormControl name="price" />
+              </FormGroup>
+              <FormGroup>
+                <ControlLabel>ImageURL</ControlLabel>
+                <FormControl name="imgUrl" />
+              </FormGroup>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <ButtonToolbar>
+              <Button
+                type="button"
+                bsStyle="primary"
+                onClick={this.handleSubmit}
+              >
+                Submit
+              </Button>
+              <Button bsStyle="link" onClick={this.hideModal}>Cancel</Button>
+            </ButtonToolbar>
+          </Modal.Footer>
+        </Modal>
+        <Toast
+          showing={toastVisible}
+          onDismiss={this.dismissToast}
+          bsStyle={toastType}
+        >
+          {toastMessage}
+        </Toast>
+      </React.Fragment>
+    );
+  }
+}
+
+export default withRouter(ProductAddNavItem);
